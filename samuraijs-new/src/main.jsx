982e03ccfdd679914711fde4868b613fd469163e
ThunderInTheGrass <propeller8924@gmail.com>
2025-12-16 16:28:49 +0800

    BrowserRouter поменян на HashRouter

--- samuraijs-new/src/main.jsx
+++ samuraijs-new/src/main.jsx
@@ -2,16 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom";
+// import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import { store } from './state/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
